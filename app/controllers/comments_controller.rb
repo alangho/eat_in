9b0ffc65a8a90c6c0a36c2321ec796245f9f557c
Alan <alangyho@gmail.com>
2015-09-01 09:06:17 -1000

    Comment deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = Micropost.find(params[:micropost_id])
@@ -18,10 +19,20 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment.destroy
+    redirect_to request.referrer || root_url
+  end
+
   private
    
-  def comment_params
-    params.require(:comment).permit(:body, :micropost_id, :picture)
-  end
+    def comment_params
+      params.require(:comment).permit(:body, :micropost_id, :picture)
+    end
+
+    def correct_user
+      @comment = current_user.comments.find_by(id: params[:id])
+      redirect_to root_url if @comment.nil?
+    end
 
 end
